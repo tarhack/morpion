5614b687dfc19cdf43d5ec46beb9facf956f2fd9
tarhack <tarhack@gmail.com>
2023-04-06 20:06:25 +0200

    TNA -> Documentation

--- src/main/java/com/codingf/morpion/Morpion.java
+++ src/main/java/com/codingf/morpion/Morpion.java
@@ -10,18 +10,20 @@ import java.util.Scanner;
 
 public class Morpion {
 
-    private static final String VERSION = "1.4.0" ;
+    private static final String VERSION = "1.4.0";
+
     private static void help() throws IOException {
-        System.out.printf("Morpion (TicTacToe) Version %s%n",VERSION);
-        Morpion.banner("banner2.txt");
+        System.out.printf("Morpion (TicTacToe) Version %s%n", VERSION);
+        Morpion.banner("banner1.txt");
         System.out.println(" ");
     }
 
     public static void display(Game game, boolean clear) throws IOException, InterruptedException {
-        if ( clear )
+        if (clear)
             DisplayConsole.clear();
         DisplayConsole.display(game);
     }
+
     public static void banner(String fileName) throws IOException {
         ClassLoader classLoader = Morpion.class.getClassLoader();
         InputStream inputStream = classLoader.getResourceAsStream(fileName);
@@ -31,38 +33,36 @@ public class Morpion {
 
     public static void main(String[] args) throws IOException, InterruptedException {
         int nbCases = 3;
-        if ( args.length > 0 ){
-            for ( int idx=0;idx< args.length;idx++){
+        if (args.length > 0) {
+            for (int idx = 0; idx < args.length; idx++) {
                 var p = args[idx];
-                if ( p.startsWith("--number")){
-                    nbCases = Integer.parseInt(args[idx+1]);
+                if (p.startsWith("--number")) {
+                    nbCases = Integer.parseInt(args[idx + 1]);
                 }
             }
         }
 
         Game game = new Game(nbCases);
         Scanner scanner = new Scanner(System.in);
-        String rep ;
-        String[] positions;
-        int line, col ;
-        String sline="=".repeat(120);
+        String rep;
+        String screenLine = "=".repeat(120);
 
 
         DisplayConsole.clear();
         help();
-        boolean clear=false;
+        boolean clear = false;
 
-        while (true){
+        while (true) {
             display(game, clear);
 
-            if(!clear)
-                clear=true;
+            if (!clear)
+                clear = true;
 
             int currentPlayer = game.getPlayTurn();
-            System.out.printf("Joueur %d , entrez la position (ligne, colonne)%n", currentPlayer);
+            System.out.printf("Joueur %d , entrez le numéro de case à jouer %n", currentPlayer);
             rep = scanner.nextLine();
 
-            if ( rep.equals(""))
+            if (rep.equals(""))
                 break;
 
 //            positions = rep.split("[ ,]");
@@ -70,49 +70,58 @@ public class Morpion {
 //                line = Integer.parseInt(positions[0].trim());
 //                col = Integer.parseInt(positions[1].trim());
 //            } catch (Exception e){
-//                System.err.println("Vous devez entrez des coordonnées valides line,col ou line col, err="+e.getLocalizedMessage());
+//                System.err.println("Vous devez entrer des coordonnées valides line, col ou line col, err="+e.getLocalizedMessage());
 //                continue;
 //            }
 //            if ( col >= nbCases || line >= nbCases ) {
-//                System.out.printf("Le nombre de ligne/colonne ne peut être supérieur %d%n",nbCases-1);
+//                System.out.printf("Le nombre de lignes/colonne ne peut être supérieur %d%n",nbCases-1);
 //                continue;
 //            }
 
             int numCase;
             try {
                 numCase = Integer.parseInt(rep);
-            } catch (Exception e){
-                System.err.println("Vous devez entrez des coordonnées valides line,col ou line col, err="+e.getLocalizedMessage());
+            } catch (Exception e) {
+                System.err.println("Vous devez entrez un numéro de case valide, err=" + e.getLocalizedMessage());
+                clear=false;
                 continue;
             }
 
-            if ( ! game.play(numCase) )
-                System.out.printf("La position %d est déjà jouée !%n",numCase);
+            if ((numCase > (nbCases * nbCases) - 1) || numCase < 0) {
+                System.out.printf("Le numéro de case est incorrect, il doit être compris entre %d et %d %n", 0, (nbCases * nbCases) - 1);
+                clear=false;
+                continue;
+            }
+
+            if (!game.play(numCase)) {
+                System.out.printf("La position %d est déjà jouée !%n", numCase);
+                clear=false;
+            }
 
             var winner = game.over();
 
-            if ( winner != 0) {
-                System.out.println(sline);
+            if (winner != 0) {
+                System.out.println(screenLine);
                 if (winner > 0) {
                     System.out.printf("Le joueur %d GAGNE !!!%n", winner);
-                    display(game,true);
-                    Morpion.banner(String.format("joueur_%d.txt",winner));
-                    System.out.println("\n"+sline);
+                    display(game, true);
+                    Morpion.banner(String.format("joueur_%d.txt", winner));
+                    System.out.println("\n" + screenLine);
                 } else {
-                    System.out.println("Situation de bloquage, on peut plus jouer !!!");
-                    System.out.println(sline);
+                    System.out.println("Situation de blocage, on peut plus jouer !!!");
+                    System.out.println(screenLine);
                 }
 
                 System.out.println("Voulez-vous refaire une partie (o, CR=Sortie) ");
                 rep = scanner.nextLine();
-                if (rep.equalsIgnoreCase("o") ){
+                if (rep.equalsIgnoreCase("o")) {
                     game.init();
                     continue;
                 } else
                     break;
             }
 
-            System.out.printf("Joueur %s joue case %d %n",currentPlayer,numCase);
+            System.out.printf("Joueur %s joue case %d %n", currentPlayer, numCase);
         }
 
         System.out.println("Fin du programme...");
